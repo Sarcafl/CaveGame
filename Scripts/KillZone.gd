540982352498a974fc3d44baf4c356b3db51846b
Sarcafl <43963692+Sarcafl@users.noreply.github.com>
2024-05-27 06:57:46 +0100

    HOPEFULLY THIS IS THE FIX
    amen at this point

--- Scripts/KillZone.gd
+++ Scripts/KillZone.gd
@@ -1,15 +1,8 @@
 extends Area2D
-@onready var death_sounds = $DeathSounds
 
 func _on_body_entered(body):
 	if body.name == "Guy":
-		death_sound()
-func death_sound():
-	if death_sounds:
-		death_sounds.play()
+		call_deferred("_reload_scene")
+
 func _reload_scene():
 	get_tree().reload_current_scene()
-
-
-func _on_death_sounds_finished():
-	_reload_scene()
