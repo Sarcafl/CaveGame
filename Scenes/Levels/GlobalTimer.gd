e1af9bb3f009e7875bca609c036191a20b1b9b13
Sarcafl <43963692+Sarcafl@users.noreply.github.com>
2024-05-27 06:05:09 +0100

    Revert "hope its not breaking things"
    This reverts commit 7b7e4a0c864b7709c8cfdf16b64114073d9e3165.

--- Scenes/Levels/GlobalTimer.gd
+++ Scenes/Levels/GlobalTimer.gd
@@ -1,31 +1,4 @@
 extends Node
 
-var speedrun_time: float = 0.0
-var timer_running: bool = false  # Initially not running
-@export var reset_scene_path = "res://Scenes/Levels/level_zero.tscn"  # Path to the scene to load when reset
+var speedrun_time = 0.0
 
-func _ready():
-	print("GlobalTimer ready, timer_running: %s" % timer_running)
-	# Ensure the timer does not start immediately
-
-func _process(delta):
-	if Input.is_action_just_pressed("reset"):
-		reset_timer()
-		get_tree().change_scene_to_file(reset_scene_path)
-	
-	if timer_running:
-		speedrun_time += delta
-	print("GlobalTimer _process: timer_running: %s, Time: %f" % [timer_running, speedrun_time])
-
-func start_timer():
-	timer_running = true
-	print("GlobalTimer start_timer called: timer_running: %s" % timer_running)
-
-func reset_timer():
-	speedrun_time = 0.0
-	timer_running = false
-	print("GlobalTimer reset_timer called: timer_running: %s" % timer_running)
-
-func stop_timer():
-	timer_running = false
-	print("GlobalTimer stop_timer called: timer_running: %s" % timer_running)
