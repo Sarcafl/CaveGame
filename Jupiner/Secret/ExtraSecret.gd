30df5ecf1ca0c57709cfa2d4929ee918acee1aab
bugjupiner <juniper@junefoley.art>
2024-05-27 14:54:35 -0400

    Level One Changes

--- Jupiner/Secret/ExtraSecret.gd
+++ Jupiner/Secret/ExtraSecret.gd
@@ -3,7 +3,7 @@ extends Node2D
 @onready var animPlayer =  $AnimationPlayer
 @onready var area = $Area2D
 
-func _on_area_2d_body_entered(body):
+func _on_area_2d_2_body_entered(body):
 	if body.name != "Guy": return
 	area.set_deferred("monitoring", false)
 	animPlayer.active = true
