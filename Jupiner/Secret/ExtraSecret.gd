d013735624ca512a3301b2872a7266a9dc839c32
bugjupiner <juniper@junefoley.art>
2024-05-27 14:42:21 -0400

    Secret Part 1

---
+++ Jupiner/Secret/ExtraSecret.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@onready var animPlayer =  $AnimationPlayer
+@onready var area = $Area2D
+
+func _on_area_2d_body_entered(body):
+	if body.name != "Guy": return
+	area.set_deferred("monitoring", false)
+	animPlayer.active = true
+	animPlayer.play("ExtraSecretAnimation")
